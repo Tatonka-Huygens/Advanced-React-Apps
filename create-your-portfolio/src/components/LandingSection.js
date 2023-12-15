eb194b0977d5b82be75c13ae5f0a1e865e132e9f
Luis Manyari <lmanyari@hotmail.com>
2023-12-15 01:19:30 -0500

    :P~ First Commit

---
+++ create-your-portfolio/src/components/LandingSection.js
@@ -0,0 +1,40 @@
+import React from "react"; 
+import { Avatar, Heading, VStack } from "@chakra-ui/react"; 
+import FullScreenSection from "./FullScreenSection"; 
+import logo from './lucho-logo.png';
+
+const greeting = "Hello, I am Lou!"; 
+const bio1 = "A Full-Stack Developer"; 
+const bio2 = "Specialized in React and Django"; 
+ 
+const LandingSection = () => ( 
+ <FullScreenSection 
+   justifyContent="center" 
+   alignItems="center" 
+   isDarkBackground 
+   backgroundColor="#2A4365" 
+ > 
+   <VStack spacing={16}> 
+     <VStack spacing={4} alignItems="center"> 
+       <Avatar 
+         src={logo}
+         size="2xl" 
+         name="Your Name" 
+       /> 
+       <Heading as="h4" size="md" noOfLines={1}> 
+         {greeting} 
+       </Heading> 
+     </VStack> 
+     <VStack spacing={6}> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio1} 
+       </Heading> 
+       <Heading as="h1" size="3xl" noOfLines={1}> 
+         {bio2} 
+       </Heading> 
+     </VStack> 
+   </VStack> 
+ </FullScreenSection> 
+); 
+ 
+export default LandingSection;
